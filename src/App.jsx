9fb5e7a7d412c39c5290fcfde16abfa1292852c5
Sethupathy <vsethupathy007@gmail.com>
2026-04-12 14:10:19 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+const name = "Sethupathy V"
+function App() {
+  return (
+    <> 
+      <h1 className="text-center font-bold color-purple-300">I am {name}, interested in web development.</h1>
+    
+    </>
+  )
+}
+
+
+export default App
