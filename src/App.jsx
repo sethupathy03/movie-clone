cf0b3e3d677450a081edd66351d95e5954770234
Sethupathy <vsethupathy007@gmail.com>
2026-04-14 11:51:32 +0530

    Final Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Movie from './components/Movie'
@@ -11,19 +11,27 @@ function App() {
   function handleAddtoWatchlist(movieObj) {
     let newWatchList = [...watchlist, movieObj]
     setWatchlist(newWatchList)
-    console.log(newWatchList)
+    localStorage.setItem('moviesApp', JSON.stringify(newWatchList))
   }
   let handleRemoveFromWatchlist = (movieObj) => {
     let filtered = watchlist.filter((movie) => movie.id !== movieObj.id);
     setWatchlist(filtered);
+    localStorage.setItem('moviesApp', JSON.stringify(filtered));
   };
+
+  useEffect(() => {
+    let moviesFromLocalStarge = localStorage.getItem('moviesApp');
+    if(!moviesFromLocalStarge) return;
+    setWatchlist(JSON.parse(moviesFromLocalStarge));
+  }, []);
+
   return (
     <> 
       <BrowserRouter>
         <Navbar />
         <Routes>
           <Route path='/' element={<> <Banner /> <Movie watchlist={watchlist} handleAddtoWatchlist={handleAddtoWatchlist} handleRemoveFromWatchlist={handleRemoveFromWatchlist} /> </>} />
-          <Route path='/watchlist' element={<Watchlist />} />
+          <Route path='/watchlist' element={<Watchlist watchlist={watchlist} setWatchlist={setWatchlist} handleRemoveFromWatchlist={handleRemoveFromWatchlist} />} />
           
         </Routes>
       </BrowserRouter>
